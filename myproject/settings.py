b3b72d362824c3f3377082aed1ac8801a9090669
parshwamehta13 <parshwamehta13@gmail.com>
2016-04-07 00:43:37 +0530

    Port number changed

--- myproject/settings.py
+++ myproject/settings.py
@@ -104,11 +104,20 @@ AUTH_PASSWORD_VALIDATORS = [
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 25
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'parshwamehta13@gmail.com'
 EMAIL_HOST_PASSWORD = '!Parshwa1995!'
 DEFAULT_FROM_EMAIL = 'parshwamehta13@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# EMAIL_USE_TLS = False
+# EMAIL_HOST = 'localhost'
+# EMAIL_HOST_USER=''
+# EMAIL_HOST_PASSWORD=''
+# EMAIL_PORT = 1025
+# DEFAULT_FROM_EMAIL = 'webmaster@localhost'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
